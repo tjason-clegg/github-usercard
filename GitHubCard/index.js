f950215fd10dcd8a7239c780b3771edc55bb70bb
Jason Clegg <tjason.clegg@gmail.com>
2020-05-07 16:04:07 -0600

    Finished all MVP requirements

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -4,6 +4,19 @@
     https://api.github.com/users/<your name>
 */
 
+
+// axios.get(`https://api.github.com/users/tjason-clegg`)
+// .then(response => {
+//   console.log(response)
+// })
+
+// .catch(error =>{
+//   console.log(error)
+// })
+
+// .finally(() => {
+//   console.log('done')
+// })
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -17,6 +30,83 @@
     and append the returned markup to the DOM as a child of .cards
 */
 
+
+const followersArray = [
+  'songamugenzi',
+  'iatechristmas',
+  'Vippsi',
+  'Jeffreyo3',
+  'bigknell',
+];
+
+const entryPoint = document.querySelector('.cards')
+
+cardMaker = (attrs) => {
+  const {avatar_url, name, username, location, profile, followers, following, bio, html_url} = attrs
+
+  const card = document.createElement('div')
+  const cardImg = document.createElement('img')
+  const cardInfo = document.createElement('div')
+  const cardTitle = document.createElement('h3')
+  const cardUsername = document.createElement('p')
+  const cardLocation = document.createElement('p')
+  const cardProfile = document.createElement('p')
+  const cardFollowers = document.createElement('p')
+  const cardFollowing = document.createElement('p')
+  const cardBio = document.createElement('p')
+  const cardLink = document.createElement('a')
+
+  entryPoint.appendChild(card)
+  card.appendChild(cardImg)
+  card.appendChild(cardInfo)
+  cardInfo.appendChild(cardTitle)
+  cardInfo.appendChild(cardUsername)
+  cardInfo.appendChild(cardLocation)
+  cardInfo.appendChild(cardProfile)
+  cardInfo.appendChild(cardFollowers)
+  cardInfo.appendChild(cardFollowing)
+  cardInfo.appendChild(cardBio)
+
+  card.classList.add('card')
+  cardInfo.classList.add('card-info')
+  cardTitle.classList.add('name')
+  cardUsername.classList.add('username')
+  
+  cardImg.src = avatar_url
+  cardTitle.textContent = name
+  cardUsername.textContent = username
+  cardLocation.textContent = `Location: ${location}`
+  cardProfile.textContent = `Profile: `
+  cardFollowers.textContent = `Followers: ${followers}`
+  cardFollowing.textContent = `Following: ${following}`
+  cardBio.textContent = `Bio ${bio}`
+  cardLink.href = html_url
+
+  cardLink.textContent = html_url
+
+  cardProfile.appendChild(cardLink)
+  return card
+
+}
+
+const getUser = (username) => {
+  axios.get(`https://api.github.com/users/${username}`)
+  .then(response => {
+
+    const user = cardMaker(response.data)
+
+    entryPoint.appendChild(user)
+    
+  })
+
+}
+
+  followersArray.forEach(follower => {
+    getUser(follower)
+  
+ })
+
+
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
     follow this link in your browser https://api.github.com/users/<Your github name>/followers,
@@ -28,7 +118,7 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
